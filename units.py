e44b22ee030375fd3cee908069184ef86469ab4c
Elias Freider <elias@freider.se>
2010-05-28 17:12:35 +0200

    Experimenting with the RandomWalker class to make it more realistic without slowing down simulations too much

--- units.py
+++ units.py
@@ -59,7 +59,7 @@ class Unit(Particle):
 			map(int, self.position), 
 			dirvector, 2)
 			
-class RandomWalker(Unit):
+class RandomWalkingAvoider(Unit):
 	step_mean = 200
 	view_range = 25
 	def __init__(self):
@@ -71,7 +71,7 @@ class RandomWalker(Unit):
 			self.waypoint_push(self.position + Vec2d(math.cos(a)*step, math.sin(a)*step))
 		
 		direction = self.waypoint().position - self.position
-		for p in view.pedestrians: #stop if any pedestrians in 
+		for p in view.pedestrians: #stop if any pedestrians are in the way
 			if direction.angle(p.position - self.position) < math.pi/2:
 				self.waypoint_clear()
 				return
@@ -81,6 +81,25 @@ class RandomWalker(Unit):
 				self.waypoint_clear()
 				return
 
+class RandomWalker(Unit):
+	step_mean = 200
+	view_range = 0
+	
+	def __init__(self):
+		Unit.__init__(self)
+		self.view_range = self.radius + 10
+		
+	def think(self, dt, view, debugsurface):		
+		if self.waypoint_len() == 0:
+			a = random.random()*math.pi*2
+			step = random.gauss(self.step_mean, self.step_mean/3)
+			self.waypoint_push(self.position + Vec2d(math.cos(a)*step, math.sin(a)*step))
+			
+		for o in view.obstacles: #avoid obstacles
+			if graphs.line_distance2(self.position, self.waypoint().position, o.p1, o.p2) <= self.radius**2:
+				self.waypoint_clear()
+				return
+				
 class Stubborn(Unit):
 	def __init__(self):
 		Unit.__init__(self)
@@ -194,14 +213,6 @@ class RoadMap(Agent):
 			for p in result.path:
 				self.waypoint_push(Vec2d(*p))
 			self.cdist = result.total_cost
-	
-class RandomTree(Agent):
-	def __init__(self):
-		super(RandomTree, self).__init__()
-		
-	@iteration
-	def think(self, dt, view, debugsurface):
-		pass
 	
 class Arty(Agent):
 	SAFETY_THRESHOLD = 0.9
