bd00797185d2bd93dcc0c5d0aadc101092718bcd
Elias Freider <elias@freider.se>
2010-08-29 17:25:42 +0200

    Example of scripted simulations (gatherstats.sh) and a RRT implementation using arty subclassing

---
+++ agents/rrt.py
@@ -0,0 +1,6 @@
+import arty
+
+class RRT(arty.Arty):
+	def future_position(self, pedestrian, time):
+		return pedestrian.position #don't extrapolate positions
+		
---
+++ gatherstats.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# ==== Arty empty MarketSquare scenario =======
+#./keiro.py -a Arty -A 0 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 5 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 10 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 15 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 20 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 30 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 40 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 50 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 60 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 70 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 80 -s MarketSquare -r 1:10
+
+# ==== Roadmap MarketSquare scenario =======
+
+#./keiro.py -a RoadMap -A 5 -s MarketSquare -r 1:10
+#./keiro.py -a RoadMap -A 10 -s MarketSquare -r 1:10
+#./keiro.py -a RoadMap -A 15 -s MarketSquare -r 1:10
+#./keiro.py -a RoadMap -A 20 -s MarketSquare -r 1:10
+./keiro.py -a RoadMap -A 30 -s MarketSquare -r 1:10
+./keiro.py -a RoadMap -A 40 -s MarketSquare -r 1:10
+./keiro.py -a RoadMap -A 50 -s MarketSquare -r 1:10
+./keiro.py -a RoadMap -A 60 -s MarketSquare -r 1:10
+./keiro.py -a RoadMap -A 70 -s MarketSquare -r 1:10
+./keiro.py -a RoadMap -A 80 -s MarketSquare -r 1:10
+
+# ==== Arty empty MarketSquare scenario =======
+#./keiro.py -a Arty -A 30 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 40 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 50 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 60 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 70 -s MarketSquare -r 1:10
+#./keiro.py -a Arty -A 80 -s MarketSquare -r 1:10
