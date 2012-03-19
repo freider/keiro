92a9fdac4088629488efc8e82362eb2d4eb05bc5
Elias Freider <freider@kth.se>
2012-03-20 00:55:59 +0100

    Tabs -> Spaces

--- mencoder.py
+++ mencoder.py
@@ -10,60 +10,62 @@ FRAME_FORMAT = "png"
 FRAME_EXTENSION = ".png"
 COMMAND_PATTERN = """mencoder mf://{frames} -mf w={width}:h={height}:fps={framerate}:type={format} -ovc \
 lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o {outfile}"""
-	
-class Encoder(object):
-	"""Encodes images to a movie file. Requires a lot of temporary hard drive space"""
-	def __init__(self):
-		self._tmpdir = tempfile.mkdtemp()
-		self.frames = 0
-		self.dim = None
-		
-	def add_frame(self, image):
-		if self.frames == 0:
-			self.dim = image.size
-		else:
-			assert(self.dim == image.size) #TODO: clean up after error!
-			
-		self.frames += 1
-		filename = os.path.join(self._tmpdir, "frame%05d%s"%(self.frames, FRAME_EXTENSION))
-		image.save(filename)
-		
-	def encode(self, framerate, path):
-		kwargs = dict(frames = os.path.join(self._tmpdir, "*"+FRAME_EXTENSION),
-			width = self.dim[0],
-			height = self.dim[1],
-			format = FRAME_FORMAT,
-			framerate = framerate,
-			outfile = path)
 
-		cmd = COMMAND_PATTERN.format(**kwargs)
-		process = subprocess.Popen(shlex.split(cmd))
-		process.communicate()
-		
-	def cleanup(self):
-		if self._tmpdir != None:
-			shutil.rmtree(self._tmpdir)
-		self._tmpdir = None
-		
-	def __del__(self):
-		self.cleanup()
-	
+
+class Encoder(object):
+    """Encodes images to a movie file. Requires a lot of temporary hard drive space"""
+    def __init__(self):
+        self._tmpdir = tempfile.mkdtemp()
+        self.frames = 0
+        self.dim = None
+
+    def add_frame(self, image):
+        if self.frames == 0:
+            self.dim = image.size
+        else:
+            assert self.dim == image.size  # TODO: clean up after error!
+
+        self.frames += 1
+        filename = os.path.join(self._tmpdir, "frame%05d%s" % (self.frames, FRAME_EXTENSION))
+        image.save(filename)
+
+    def encode(self, framerate, path):
+        kwargs = dict(frames=os.path.join(self._tmpdir, "*" + FRAME_EXTENSION),
+            width=self.dim[0],
+            height=self.dim[1],
+            format=FRAME_FORMAT,
+            framerate=framerate,
+            outfile=path)
+
+        cmd = COMMAND_PATTERN.format(**kwargs)
+        process = subprocess.Popen(shlex.split(cmd))
+        process.communicate()
+
+    def cleanup(self):
+        if self._tmpdir != None:
+            shutil.rmtree(self._tmpdir)
+        self._tmpdir = None
+
+    def __del__(self):
+        self.cleanup()
+
+
 class TestEncoder(unittest.TestCase):
-	def testencode(self):
-		enc = Encoder()
-		size = (640,480)
-		mode = "RGB"
-		framerate = 2
-		red = Image.new(mode, size, (255, 0, 0))
-		blue = Image.new(mode, size, (0, 0, 255))
-		
-		enc.add_frame(red)
-		enc.add_frame(blue)
-		enc.add_frame(red)
-		enc.add_frame(blue)
-		
-		enc.encode(framerate, "/Users/elias/Desktop/test.avi") #TODO: more flexible testing
-		enc.cleanup()
-	
+    def testencode(self):
+        enc = Encoder()
+        size = (640, 480)
+        mode = "RGB"
+        framerate = 2
+        red = Image.new(mode, size, (255, 0, 0))
+        blue = Image.new(mode, size, (0, 0, 255))
+
+        enc.add_frame(red)
+        enc.add_frame(blue)
+        enc.add_frame(red)
+        enc.add_frame(blue)
+
+        enc.encode(framerate, "/Users/elias/Desktop/test.avi")  # TODO: more flexible testing
+        enc.cleanup()
+
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
--- world.py
+++ world.py
@@ -1,119 +1,118 @@
-import time
-import random
 import pygame
 import sys
-import math
-import os
 import Image
 
-from fast.physics import Vec2d, Particle, World as PhysicsWorld
+from fast.physics import World as PhysicsWorld
+
 
 class View(object):
-	def __init__(self, obstacles, pedestrians):
-		self.obstacles = obstacles
-		self.pedestrians = pedestrians
-		self.convex_hull = []
+    def __init__(self, obstacles, pedestrians):
+        self.obstacles = obstacles
+        self.pedestrians = pedestrians
+        self.convex_hull = []
 
-	def add_ch(self, ch):
-		self.convex_hull = ch
-		
+    def add_ch(self, ch):
+        self.convex_hull = ch
+
+
 class World(PhysicsWorld):
-	def __init__(self, size, opts):
-		PhysicsWorld.__init__(self)
-		self.size = size		
-		self.units = []
-		self.obstacles = []
+    def __init__(self, size, opts):
+        PhysicsWorld.__init__(self)
+        self.size = size
+        self.units = []
+        self.obstacles = []
 
-		self.clock = pygame.time.Clock()
-		self.timestep = opts.timestep
-		self.fps = opts.fps
-		self.encoders = []
+        self.clock = pygame.time.Clock()
+        self.timestep = opts.timestep
+        self.fps = opts.fps
+        self.encoders = []
 
-		self.collision_list = []
-		self.avg_groundspeed_list = []
-		
-	def add_unit(self, unit):
-		self.units.append(unit)
-		self.bind(unit)
-	
-	def remove_unit(self, unit):
-		self.units.remove(unit)
-		self.unbind(unit)
+        self.collision_list = []
+        self.avg_groundspeed_list = []
 
-	def add_obstacle(self, obstacle):
-		self.obstacles.append(obstacle)
-		for line in obstacle.bounds:
-			self.bind(line)
-		
-	def remove_obstacle(self, obstacle):
-		for line in obstacle.bounds:
-			self.unbind(line)
-		self.obstacles.remove(obstacle)
-		
-	def add_encoder(self, encoder):
-		self.encoders.append(encoder)
-		
-	def init(self):
-		pygame.init()
-		pygame.display.set_caption("Crowd Navigation")
-		self._screen = pygame.display.set_mode(self.size)
-		self._time = 0
-		self._iterations = 0
-		self.update(0) #so we have no initial collisions
-		self.debugsurface = pygame.Surface(self.size, masks = pygame.SRCALPHA).convert_alpha()
-	
-	def get_time(self):
-		return self._time
-	
-	def advance(self):
-		if self.timestep == 0:
-			dt = self.clock.tick()/1000.0 #use real time
-		else:
-			self.clock.tick()
-			dt = self.timestep
-			
-		self._time += dt
-		self._iterations += 1
-		
-		self.update(dt)
-		
-		self.debugsurface.fill((0,255,0,0))
-		
-		for u in self.units:
-			if u.view_range != 0:
-				#view = View(self.get_obstacles(), self.particles_in_range(u, u.view_range))
-				view = View(self.get_obstacles(), self.particles_in_view_range(u, u.view_range)) #(with occlusion)
-			else:
-				view = View(self.get_obstacles(), [])
-				
-			u.think(dt, view, self.debugsurface)
-	
-		if self.fps:
-			sys.stdout.write("%f fps           \r"%self.clock.get_fps())
-			sys.stdout.flush()
-		
-		self.render(self._screen)
-		return dt
-			
-	def render(self, screen):
-		screen.fill((255,255,255))
-			
-		for o in self.obstacles:
-			o.render(screen)
-		ID = 0
-		for u in self.units:
-			#u.render_ID(screen, ID)
-			u.render(screen)
-			ID = ID + 1
+    def add_unit(self, unit):
+        self.units.append(unit)
+        self.bind(unit)
 
-		screen.blit(self.debugsurface, (0, 0))
+    def remove_unit(self, unit):
+        self.units.remove(unit)
+        self.unbind(unit)
 
-		pygame.display.flip()
-	
-		if len(self.encoders) > 0:
-			mode = "RGB"
-			imagestring = pygame.image.tostring(screen, mode)
-			image = Image.fromstring(mode, self.size, imagestring)
-			
-			for enc in self.encoders:
-				enc.add_frame(image)
+    def add_obstacle(self, obstacle):
+        self.obstacles.append(obstacle)
+        for line in obstacle.bounds:
+            self.bind(line)
+
+    def remove_obstacle(self, obstacle):
+        for line in obstacle.bounds:
+            self.unbind(line)
+        self.obstacles.remove(obstacle)
+
+    def add_encoder(self, encoder):
+        self.encoders.append(encoder)
+
+    def init(self):
+        pygame.init()
+        pygame.display.set_caption("Crowd Navigation")
+        self._screen = pygame.display.set_mode(self.size)
+        self._time = 0
+        self._iterations = 0
+        self.update(0)  # so we have no initial collisions
+        self.debugsurface = pygame.Surface(self.size, masks=pygame.SRCALPHA).convert_alpha()
+
+    def get_time(self):
+        return self._time
+
+    def advance(self):
+        if self.timestep == 0:
+            dt = self.clock.tick() / 1000.0  # use real time
+        else:
+            self.clock.tick()
+            dt = self.timestep
+
+        self._time += dt
+        self._iterations += 1
+
+        self.update(dt)
+
+        self.debugsurface.fill((0, 255, 0, 0))
+
+        for u in self.units:
+            if u.view_range != 0:
+                #view = View(self.get_obstacles(), self.particles_in_range(u, u.view_range))
+                # Marc's occlusion code
+                view = View(self.get_obstacles(), self.particles_in_view_range(u, u.view_range))  # (with occlusion)
+            else:
+                view = View(self.get_obstacles(), [])
+
+            u.think(dt, view, self.debugsurface)
+
+        if self.fps:
+            sys.stdout.write("%f fps           \r" % self.clock.get_fps())
+            sys.stdout.flush()
+
+        self.render(self._screen)
+        return dt
+
+    def render(self, screen):
+        screen.fill((255, 255, 255))
+
+        for o in self.obstacles:
+            o.render(screen)
+        ID = 0
+        for u in self.units:
+            #u.render_ID(screen, ID)
+            u.render(screen)
+            ID = ID + 1
+
+        screen.blit(self.debugsurface, (0, 0))
+
+        pygame.display.flip()
+
+        if len(self.encoders) > 0:
+            mode = "RGB"
+            imagestring = pygame.image.tostring(screen, mode)
+            image = Image.fromstring(mode, self.size, imagestring)
+
+            for enc in self.encoders:
+                enc.add_frame(image)
